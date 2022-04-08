5fc1369043c3c464e9e2809ed687f337bb8d437d
Avishka Indula <avishkainduladev@gmail.com>
2022-04-08 08:22:07 +0530

    linking external JS files to the HTML file

---
+++ app.js
@@ -0,0 +1,26 @@
+alert(20);
+alert("Hi, I'm Indula!");
+// alert('Hi, I\'m Indula!');
+// to use a ' mark inside a string, we need to add \ mark before '
+
+let greetingText;
+greetingText = "Hi, I'm a self taught developer!";
+// "let" is the keyword to create variables.
+// variable should be a "one word" without any - marks or any other special character.
+// we should start with a lower case character, and then use upper case characters in the beginning of other sub-names.
+// this is known as "camel case".
+alert(greetingText);
+
+// we can also assign a value to a variable in the "same line" where the "let" keyword stands.
+let nextText = "I like programming!";
+alert(nextText);
+alert(nextText);
+
+// we can also "overwrite" the value of the nextText variable.
+// The new value is assigned to the nextText variable "after" the first value is assigned and executed to it.
+nextText = "I like to produce music to!";
+alert(nextText);
+
+let myAge;
+myAge = 20;
+alert(myAge);
